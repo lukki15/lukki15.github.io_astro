7b8986a410e77fe6e25ffcc91c736d287c1a75c8
lukki15 <50550773+lukki15@users.noreply.github.com>
2023-07-22 22:16:43 +0200

    personalice config and settings

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
-	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	title: `lukki15 homepage`,
+	description: `Personal website to share my thoughts, experiences, and projects with the world.`,
+	url: `https://lukki15.github.io`, // No trailing slash!
+	name: `lukki15`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
   showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
